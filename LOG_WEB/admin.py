ba4354248e75f0c7c3f2a3ae2efaaa61d671eb67
lgustavoss <gustaavojaco@gmail.com>
2020-07-29 17:14:35 -0300

    Atualização

--- LOG_WEB/admin.py
+++ LOG_WEB/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Motorista
+from .models import *
 
 # Register your models here.
 
 
 admin.site.register(Motorista)
+admin.site.register(Estabelecimento)
+admin.site.register(Colaborador)
--- LOG_WEB/models.py
+++ LOG_WEB/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 
 class Motorista(models.Model):
     STATUS_CHOICES = [
-        ('Ativo', 'ativo'),
-        ('Inativo', 'inativo'),
+        ('Ativo', 'Ativo'),
+        ('Inativo', 'Inativo'),
     ]
     nome = models.CharField(max_length=80)
     cpf = models.CharField(max_length=20, null=False, unique=True)
@@ -19,4 +19,32 @@ class Motorista(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Estabelecimento(models.Model):
+    STATUS_CHOICES = [
+        ('Ativo', 'Ativo'),
+        ('Inativo', 'Inativo'),
+    ]
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='Ativo')
+    nome = models.CharField(max_length=80)
+    cnpj = models.CharField(max_length=20, null=False, unique=True)
+    cidade = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+
+class Colaborador(models.Model):
+    STATUS_CHOICES = [
+        ('Ativo', 'Ativo'),
+        ('Inativo', 'Inativo'),
+    ]
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='Ativo')
+    nome = models.CharField(max_length=100)
+    funcao = models.CharField(max_length=80)
+    email = models.EmailField(max_length=254, blank=True)
+    estabelecimento = models.ForeignKey(Estabelecimento, on_delete=models.CASCADE)
+    observacao = models.TextField(blank=True)
+
 
